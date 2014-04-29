7969577210fbf50b41df7e833aa5de4108c5c481
Jose Antonio Chavarría <jachavar@gmail.com>
2014-04-29 08:18:21 +0200

    First release

---
+++ usr/lib/firefox/vx-kiosk-global-conf.js
@@ -0,0 +1,8 @@
+lockPref("config.use_system_prefs", false);
+lockPref("app.update.enabled", false);
+lockPref("browser.rights.3.shown", true);
+lockPref("browser.search.update", false);
+lockPref("browser.startup.homepage", "http://www.zaragoza.es/");
+lockPref("browser.startup.homepage_override.mstone", "ignore");
+lockPref("extensions.update.enabled", false);
+lockPref("toolkit.telemetry.prompted", true);
